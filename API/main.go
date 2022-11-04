1b35f39b8163772b69f752c8eae79b5698ee2c0e
Leo <danielesquivel00.lded@gmail.com>
2022-11-04 09:13:39 -0600

    Cors agregado

--- API/main.go
+++ API/main.go
@@ -5,12 +5,23 @@ import (
 
 	"github.com/go-chi/chi/v5"
 
+	"github.com/go-chi/cors"
+
 	"api/handler"
 )
 
 func main() {
 	port := ":3000"
 	r := chi.NewRouter()
+
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 
 	r.Route("/api", func(r chi.Router) {
 		r.Get("/{from}-{limit}", handler.GetLastMails())
